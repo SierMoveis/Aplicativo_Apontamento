d684bd3a864635ce49af2207b6ce4ec794a64794
SierMoveis <suportesier@gmail.com>
2025-01-30 14:42:08 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -358,7 +358,7 @@ const unidades = {
             { data: "24/01/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Cnc/24.jpg","capturas/Unidade 1/2025/Usinagem/Filetação/24.jpg"] },
             { data: "27/01/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/Janeiro/27.jpg", "capturas/Unidade 1/2025/Usinagem/Cnc/27.jpg"] },
             { data: "28/01/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Filetação/28.jpg", "capturas/Unidade 1/2025/Usinagem/Cnc/28.jpg"] },
-            { data: "29/01/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Cnc/29.jpg", ""] },
+            { data: "29/01/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Cnc/29.jpg", "capturas/Unidade 1/2025/Usinagem/Filetação/29.jpg"] },
             
             // { data: "", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/Janeiro/29.jpg"] },
         
