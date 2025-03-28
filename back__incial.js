9c5440933bdcf5be3e4dfe3a19ab3969bb153d99
SierMoveis <suportesier@gmail.com>
2025-03-28 14:10:32 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -1053,7 +1053,7 @@ const unidades = {
             { data: "24/03/2025", captura: ["capturas/Unidade 2/Usinagem/2025/Março/Mont.Us/24.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/Rolinho/24.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/Roquite/24.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/MM/24.png", "capturas/Unidade 2/Usinagem/2025/Março/Usinagem/24.png"] },
             { data: "25/03/2025", captura: ["capturas/Unidade 2/Usinagem/2025/Março/Mont.Us/25.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/Rolinho/25.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/Roquite/25.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/MM/25.jpg", "capturas/Unidade 2/Usinagem/2025/Março/Usinagem/25.png"] },
             { data: "26/03/2025", captura: ["capturas/Unidade 2/Usinagem/2025/Março/Mont.Us/26.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/Rolinho/26.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/Roquite/26.jpeg", "capturas/Unidade 2/Usinagem/2025/Março/MM/25.png", "capturas/Unidade 2/Usinagem/2025/Março/Usinagem/26.png"] },
-
+            { data: "27/03/2025", captura: ["capturas/Unidade 2/Usinagem/2025/Março/Mont.Us/27.png", "capturas/Unidade 2/Usinagem/2025/Março/Rolinho/27.png", "capturas/Unidade 2/Usinagem/2025/Março/Roquite/27.png", "capturas/Unidade 2/Usinagem/2025/Março/MM/27.png", "capturas/Unidade 2/Usinagem/2025/Março/Usinagem/27.png"] },
 
 
 
