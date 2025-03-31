c60c461ec1a6b33c5c4be51b73a1275a9eae1228
SierMoveis <suportesier@gmail.com>
2025-03-31 11:07:32 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -927,9 +927,13 @@ const unidades = {
             { data: "25/03/2025", captura: ["capturas/Unidade 2/Usinagem/2025/Março/CNC/25.jpeg"] },
             { data: "26/03/2025", captura: ["capturas/Unidade 2/Usinagem/2025/Março/CNC/26.jpeg"] },
             { data: "27/03/2025", captura: ["capturas/Unidade 2/Usinagem/2025/Março/CNC/27.jpeg"] },
+            { data: "28/03/2025", captura: ["capturas/Unidade 2/Usinagem/2025/Março/CNC/28.jpeg"] },
+
+
 
 
         ],
+
         // { data: "", captura: ["", "", "", "", ""] },
         // { data: "", captura: ["", ""] },
 
@@ -1284,7 +1288,8 @@ const unidades = {
             { data: "25/03/2025", captura: ["capturas/Unidade 3/Usinagem/2025/03-MARÇO/25.png"] }, 
             { data: "26/03/2025", captura: ["capturas/Unidade 3/Usinagem/2025/03-MARÇO/26.png"] }, 
             { data: "27/03/2025", captura: ["capturas/Unidade 3/Usinagem/2025/03-MARÇO/27.png"] }, 
-
+            { data: "28/03/2025", captura: ["capturas/Unidade 3/Usinagem/2025/03-MARÇO/28.png"] }, 
+      
 
 
 
