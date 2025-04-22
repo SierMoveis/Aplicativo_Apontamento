4ca3bdc37e561892a5e1b152bcd5a2e0f141f06c
SierMoveis <suportesier@gmail.com>
2025-04-22 15:04:42 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -353,7 +353,7 @@ const unidades = {
             { data: "14/04/2025", captura: ["capturas/Unidade 1/2025/Stone/ABRIL/14.jpeg", "capturas/Unidade 1/2025/LixaçãoOsso/ABRIL/14.jpeg", "capturas/Unidade 1/2025/LixaçãoMetal/ABRIL/14.jpeg", "capturas/Unidade 1/2025/Lix.Fundo/ABRIL/14.jpeg"] },
             { data: "15/04/2025", captura: ["capturas/Unidade 1/2025/Stone/ABRIL/15.jpeg", "capturas/Unidade 1/2025/LixaçãoOsso/ABRIL/15.jpeg", "capturas/Unidade 1/2025/LixaçãoMetal/ABRIL/15.jpeg", "capturas/Unidade 1/2025/Lix.Fundo/ABRIL/15.jpeg"] },
             { data: "16/04/2025", captura: ["capturas/Unidade 1/2025/Stone/ABRIL/16.jpeg", "capturas/Unidade 1/2025/LixaçãoOsso/ABRIL/16.jpeg", "capturas/Unidade 1/2025/LixaçãoMetal/ABRIL/16.jpeg", "capturas/Unidade 1/2025/Lix.Fundo/ABRIL/16.jpeg"] },
-            { data: "17/04/2025", captura: ["capturas/Unidade 1/2025/Stone/ABRIL/17.jpeg", "", "capturas/Unidade 1/2025/LixaçãoMetal/ABRIL/17.jpeg", ""] },
+            { data: "17/04/2025", captura: ["capturas/Unidade 1/2025/Stone/ABRIL/17.jpeg", "capturas/Unidade 1/2025/LixaçãoOsso/ABRIL/17.jpeg", "capturas/Unidade 1/2025/LixaçãoMetal/ABRIL/17.jpeg", ""] },
 
 
             // { data: "", captura: ["", "", "", ""] },
