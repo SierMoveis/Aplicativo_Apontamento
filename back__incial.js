76e2535ffb828dce1acadc02a3f5dbed88847356
SierMoveis <suportesier@gmail.com>
2025-04-24 15:26:47 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -757,7 +757,7 @@ const unidades = {
             { data: "16/04/2025", captura: ["capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/16-pt1.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/16-pt2.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/16-pt3.jpeg"] },
             { data: "17/04/2025", captura: ["capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/17-pt1.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/17-pt2.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/17-pt3.jpeg"] },
             { data: "22/04/2025", captura: ["capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/22-pt1.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/22-pt2.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/22-pt3.jpeg"] },
-
+            { data: "23/04/2025", captura: ["capturas/Unidade 2/Estofaria/2025/Bordadeira/04-ABRIL/23.jpg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/23-pt1.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/23-pt2.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/04-ABRIL/23-pt3.jpeg"] },
 
 
 
@@ -899,6 +899,10 @@ const unidades = {
             { data: "16/04/2025", captura: ["capturas/Unidade 2/Expedição/2025/04-ABRIL/16.jpeg"] },
             { data: "17/04/2025", captura: ["capturas/Unidade 2/Expedição/2025/04-ABRIL/17.jpg"] },
             { data: "22/04/2025", captura: ["capturas/Unidade 2/Expedição/2025/04-ABRIL/22.jpg"] },
+            { data: "23/04/2025", captura: ["capturas/Unidade 2/Expedição/2025/04-ABRIL/23.jpg"] },
+
+
+
 
 
             // { data: "", captura: [""] },
