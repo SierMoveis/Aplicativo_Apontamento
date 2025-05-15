d63f53781816036bbdc2d036fdbadfbde3ade5aa
SierMoveis <suportesier@gmail.com>
2025-05-15 09:49:09 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -526,6 +526,8 @@ const unidades = {
             { data: "09/05/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Tupia/05 MAIO/09.jpg", "capturas/Unidade 1/2025/Usinagem/Filetação/05-MAIO/09.jpg"] },
             { data: "12/05/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Tupia/05 MAIO/12.jpg", "capturas/Unidade 1/2025/Usinagem/Filetação/05-MAIO/12.jpg"] },
             { data: "13/05/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Tupia/05 MAIO/13.jpg", "capturas/Unidade 1/2025/Usinagem/Filetação/05-MAIO/13.jpg"] },
+            { data: "14/05/2025", captura: ["capturas/capturas/Unidade 1/2025/Usinagem/Tupia/05 MAIO/14.jpg", "capturas/capturas/Unidade 1/2025/Usinagem/Filetação/05-MAIO/14.jpg"] },
+
 
             // { data: "", captura: ["", ""] },
 
@@ -668,6 +670,7 @@ const unidades = {
             { data: "09/05/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/5-MAIO/09.jpg"] },
             { data: "12/05/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/5-MAIO/12.jpg"] },
             { data: "13/05/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/5-MAIO/13.jpg"] },
+            { data: "14/05/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/5-MAIO/14.jpg"] },
 
 
             // { data: "", captura: [""] },
