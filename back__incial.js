27321ffaf385fd6b97d813a0b76386d9407cd7a4
SierMoveis <suportesier@gmail.com>
2025-05-20 11:46:12 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -1001,7 +1001,7 @@ const unidades = {
             { data: "14/05/2025", captura: ["capturas/Unidade 2/Expedição/2025/05-MAIO/14.jpg"] },
             { data: "15/05/2025", captura: ["capturas/Unidade 2/Expedição/2025/05-MAIO/15.jpg"] },
             { data: "16/05/2025", captura: ["capturas/Unidade 2/Expedição/2025/05-MAIO/16.jpg"] },
-
+            { data: "19/05/2025", captura: ["capturas/Unidade 2/Expedição/2025/05-MAIO/19.jpg"] },
 
 
 
