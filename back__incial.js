f593bfd163aea50a8e4fb1d7096246b1115f6d00
SierMoveis <suportesier@gmail.com>
2025-06-23 13:46:00 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -257,7 +257,9 @@ const unidades = {
             { data: "16/06/2025", captura: ["capturas/Unidade 1/2025/Expedição/MetasTv/JUNHO/16.jpg", "capturas/Unidade 1/2025/Expedição/Embalagem/JUNHO/16.jpg"] },
             { data: "17/06/2025", captura: ["capturas/Unidade 1/2025/Expedição/MetasTv/JUNHO/17.jpg", "capturas/Unidade 1/2025/Expedição/Embalagem/JUNHO/17.jpg"] },
             { data: "18/06/2025", captura: ["capturas/Unidade 1/2025/Expedição/MetasTv/JUNHO/18.jpg", "capturas/Unidade 1/2025/Expedição/Embalagem/JUNHO/18.jpg"] },
-              
+            { data: "19/06/2025", captura: ["capturas/Unidade 1/2025/Expedição/MetasTv/JUNHO/19.jpg", "capturas/Unidade 1/2025/Expedição/Embalagem/JUNHO/19.jpg"] },
+             
+             
             // { data: "", captura: ["", ""] },
 
 
