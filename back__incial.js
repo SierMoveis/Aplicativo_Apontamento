b33e7ccd186dc7934b034e0f37a4b5e73c5a8167
SierMoveis <suportesier@gmail.com>
2025-06-23 13:20:58 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -596,6 +596,7 @@ const unidades = {
             { data: "16/06/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Filetação/06-JUNHO/16.jpg"] },
             { data: "17/06/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Filetação/06-JUNHO/17.jpg"] },
             { data: "18/06/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Filetação/06-JUNHO/18.jpg"] },
+            { data: "19/06/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Filetação/06-JUNHO/19.jpg"] },
 
 
 
@@ -763,7 +764,7 @@ const unidades = {
             { data: "16/06/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/06-JUNHO/16.jpg"] },
             { data: "17/06/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/06-JUNHO/17.jpg"] },
             { data: "18/06/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/06-JUNHO/18.jpg"] },
-
+            { data: "19/06/2025", captura: ["capturas/Unidade 1/2025/Usinagem/Montagem/06-JUNHO/19.jpg"] },
 
             // { data: "", captura: [""] },
         ],
