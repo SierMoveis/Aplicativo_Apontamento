27ce2e6901d5ea5c5d87a9892d5ab06cfc4e4e9d
SierMoveis <suportesier@gmail.com>
2025-08-21 11:03:19 -0300

    ,,

--- back__incial.js
+++ back__incial.js
@@ -1841,7 +1841,7 @@ const unidades = {
             { data: "15/08/2025", captura: ["capturas/Unidade 2/Usinagem/2025/AGOSTO/MONTAGEM US/15.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/ROLINHO/15.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/ROQUITE/15.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/MM/15.jpg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/USINAGEM/15.jpg"] },
             { data: "18/08/2025", captura: ["capturas/Unidade 2/Usinagem/2025/AGOSTO/MONTAGEM US/18.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/ROLINHO/18.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/ROQUITE/18.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/MM/18.jpg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/USINAGEM/18.jpg"] },
             { data: "19/08/2025", captura: ["capturas/Unidade 2/Usinagem/2025/AGOSTO/MONTAGEM US/19.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/ROLINHO/19.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/ROQUITE/19.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/MM/19.jpg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/USINAGEM/19.jpg"] },
-            { data: "20/08/2025", captura: ["capturas/Unidade 2/Usinagem/2025/AGOSTO/MONTAGEM US/20.jpeg", "", "", "capturas/Unidade 2/Usinagem/2025/AGOSTO/MM/19.jpg", ""] },
+            { data: "20/08/2025", captura: ["capturas/Unidade 2/Usinagem/2025/AGOSTO/MONTAGEM US/20.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/ROLINHO/20.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/ROQUITE/20.jpeg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/MM/19.jpg", "capturas/Unidade 2/Usinagem/2025/AGOSTO/USINAGEM/20.jpg"] },
 
 
 
