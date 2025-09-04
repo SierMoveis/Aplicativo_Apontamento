002709cd8ea981451a468c479882de997cdf0499
SierMoveis <suportesier@gmail.com>
2025-09-04 14:35:46 -0300

    ,

--- back__incial.js
+++ back__incial.js
@@ -1201,7 +1201,7 @@ const unidades = {
             
             { data: "01/09/2025", captura: ["capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/01-pt1.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/01-pt2.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/01-pt3.jpeg", "capturas/Unidade 2/Estofaria/2025/Bordadeira/09-SETEMBRO/01.jpg"] },
             { data: "02/09/2025", captura: ["capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/02-pt1.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/02-pt2.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/02-pt3.jpeg"] },
-
+            { data: "03/09/2025", captura: ["capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/03-pt1.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/03-pt2.jpeg", "capturas/Unidade 2/Estofaria/2025/Estofaria/09-SETEMBRO/03-pt3.jpeg", "capturas/Unidade 2/Estofaria/2025/Bordadeira/09-SETEMBRO/03.jpg"] },
 
 
 
