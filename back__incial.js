9fc585d7d0fec9af0f10872649575ecd72f52749
SierMoveis <suportesier@gmail.com>
2025-10-07 13:14:44 -0300

    ,,,

--- back__incial.js
+++ back__incial.js
@@ -4634,7 +4634,7 @@ const unidades = {
             { data: "12/09/2025", captura: ["capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/12 pt01.jpg", "capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/12 pt02.jpg", "capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/12g.jpg"] },
             { data: "18/09/2025", captura: ["capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/18 pt01.jpg", "capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/18 pt02.jpg", "capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/18 g.jpg"] },
             { data: "26/09/2025", captura: ["capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/26 pt01.jpg", "capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/26 pt02.jpg", "capturas/Retrabalhos/2025/SETEMBRO/Unidade 2/Curvados/26 g.jpg"] },
-            { data: "03/10/2025", captura: ["", "", ""] },
+            { data: "03/10/2025", captura: ["capturas/Retrabalhos/2025/OUTUBRO/Unidade 2/Curvados/03 pt01.jpg", "capturas/Retrabalhos/2025/OUTUBRO/Unidade 2/Curvados/03 pt02.jpg", "capturas/Retrabalhos/2025/OUTUBRO/Unidade 2/Curvados/3g.jpg"] },
            
            
 
