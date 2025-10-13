129220b63839b22a92e66874a015626ba391a0cd
SierMoveis <suportesier@gmail.com>
2025-10-13 13:52:35 -0300

    ,,

--- back__incial.js
+++ back__incial.js
@@ -2799,6 +2799,9 @@ const unidades = {
             { data: "07/10/2025", captura: ["capturas/Unidade 4/Usinagem/2025/Calibradora/OUTUBRO/07.jpg"] },
             { data: "08/10/2025", captura: ["capturas/Unidade 4/Usinagem/2025/Calibradora/OUTUBRO/08.jpg"] },
             { data: "09/10/2025", captura: ["capturas/Unidade 4/Usinagem/2025/Calibradora/OUTUBRO/09.jpg"] },
+            { data: "10/10/2025", captura: ["capturas/Unidade 4/Usinagem/2025/Calibradora/OUTUBRO/10.jpg"] },
+
+
 
 
             // { data: "", captura: [""] },
@@ -2971,7 +2974,7 @@ const unidades = {
             { data: "07/10/2025", captura: ["capturas/Unidade 4/Usinagem/2025/Prensas/OUTUBRO/07.jpg"] },
             { data: "08/10/2025", captura: ["capturas/Unidade 4/Usinagem/2025/Prensas/OUTUBRO/08.jpg"] },
             { data: "09/10/2025", captura: ["capturas/Unidade 4/Usinagem/2025/Prensas/OUTUBRO/09.jpg"] },
-
+            { data: "10/10/2025", captura: ["capturas/Unidade 4/Usinagem/2025/Prensas/OUTUBRO/10.jpg"] },
 
 
              // { data: "", captura: [""] },
@@ -3686,7 +3689,10 @@ const unidades = {
             { data: "07/10/2025", captura: ["capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/MAQ 07.jpg", "capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/ITALP 07.jpg", "capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/HOME 07.jpg"] },
             { data: "08/10/2025", captura: ["capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/MAQ 08.jpg", "capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/ITALP 08.jpg", "capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/HOME 08.jpg"] },
             { data: "09/10/2025", captura: ["capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/MAQ 09.jpg", "capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/ITALP 09.jpg", "capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/HOME 09.jpg"] },
-            
+            { data: "10/10/2025", captura: ["capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/MAQ 10.jpg", "capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/ITALP 10.jpg", "capturas/Unidade 4/Tempo_parado/2025/OUTUBRO/HOME 10.jpg"] },
+
+
+
 
             // { data: "", captura: ["", "", ""] },
 
