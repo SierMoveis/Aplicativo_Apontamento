432b7981411d06dfd5a988d48afeebb2316be79c
SierMoveis <suportesier@gmail.com>
2025-10-23 16:23:34 -0300

    ,,

--- back__incial.js
+++ back__incial.js
@@ -2408,6 +2408,7 @@ const unidades = {
             { data: "17/10/2025", captura: ["capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/17 PT1.jpg", "capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/17 PT2.jpg"] },
             { data: "20/10/2025", captura: ["capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/20  PT1.jpg", "capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/20  PT2.jpg"] },
             { data: "21/10/2025", captura: ["capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/21 PT1.jpg", "capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/21 PT2.jpg", "capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/21 PT1h.jpg", "capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/21 PT2h.jpg"] },
+            { data: "22/10/2025", captura: ["capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/22 PT1.jpg", "capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/22 PT2.jpg", "capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/22 PT1h.jpg", "capturas/Unidade 2/Acabamento/2025/ACABAMENTO/OUTUBRO/22 PT2h.jpg"] },
 
 
 
