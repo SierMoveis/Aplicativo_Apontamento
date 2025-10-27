4602ce1b99b1329f006b95f8cc89e4ee12f21415
SierMoveis <suportesier@gmail.com>
2025-10-27 13:19:32 -0300

    ,,

--- back__incial.js
+++ back__incial.js
@@ -2195,8 +2195,10 @@ const unidades = {
             { data: "21/10/2025", captura: ["capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MONTAGEM US/21.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROLINHO/21.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROQUITE/21.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MM/21.jpg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/USINAGEM/21.jpg"] },
             { data: "22/10/2025", captura: ["capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MONTAGEM US/22.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROLINHO/22.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROQUITE/22.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MM/22.jpg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/USINAGEM/22.jpg"] },
             { data: "23/10/2025", captura: ["capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MONTAGEM US/23.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROLINHO/23.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROQUITE/23.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MM/23.jpg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/USINAGEM/23.jpg"] },
-            { data: "24/10/2025", captura: ["capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MONTAGEM US/24.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROLINHO/24.jpeg", "", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MM/24.jpg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/USINAGEM/27.jpeg"] },
+            { data: "24/10/2025", captura: ["capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MONTAGEM US/24.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROLINHO/24.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/ROQUITE/24.jpeg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/MM/24.jpg", "capturas/Unidade 2/Usinagem/2025/10-OUTUBRO/USINAGEM/27.jpeg"] },
 
+
+            
 
             ],
 
