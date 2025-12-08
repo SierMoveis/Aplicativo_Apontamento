50602251d684d53b221af1a9c69a041f3a419212
SierMoveis <suportesier@gmail.com>
2025-12-08 10:33:34 -0300

    ,,

--- back__incial.js
+++ back__incial.js
@@ -2377,7 +2377,7 @@ const unidades = {
             { data: "03/12/2025", captura: ["capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/MONTAGEM US/03.JPEG", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/ROLINHO/03.jpeg", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/ROQUITE/03.JPEG", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/MM/03.jpg", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/USINAGEM/03.jpg"] },
             { data: "04/12/2025", captura: ["capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/MONTAGEM US/04.JPEG", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/ROLINHO/04.jpeg", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/ROQUITE/04.JPEG", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/MM/04.jpg", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/USINAGEM/04.jpg"] },
             { data: "05/12/2025", captura: ["capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/MONTAGEM US/05.JPEG", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/ROLINHO/05.jpeg", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/ROQUITE/05.JPEG", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/MM/05.jpg", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/USINAGEM/05.jpg"] },
-
+            { data: "06/12/2025", captura: ["capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/MONTAGEM US/06.JPEG", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/ROLINHO/06.jpeg", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/ROQUITE/06.JPEG", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/MM/06.jpg", "capturas/Unidade 2/Usinagem/2025/12-DEZEMBRO/USINAGEM/06.jpg"] },
 
 
 
@@ -2935,7 +2935,7 @@ const unidades = {
             { data: "03/12/2025", captura: ["capturas/Unidade 3/Usinagem/2025/12-DEZEMBRO/03.jpg"] }, 
             { data: "04/12/2025", captura: ["capturas/Unidade 3/Usinagem/2025/12-DEZEMBRO/04.jpg"] }, 
             { data: "05/12/2025", captura: ["capturas/Unidade 3/Usinagem/2025/12-DEZEMBRO/05.jpg"] }, 
-
+            { data: "06/12/2025", captura: ["capturas/Unidade 3/Usinagem/2025/12-DEZEMBRO/06.jpg"] },
 
 
 
