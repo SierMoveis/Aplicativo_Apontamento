f5fec22463e3b18dcadc32e6e3453177fec7bbff
SierMoveis <suportesier@gmail.com>
2026-01-15 15:51:35 -0300

    ,,

--- back__incial.js
+++ back__incial.js
@@ -394,8 +394,11 @@ const unidades = {
             { data: "09/01/2026", captura: ["capturas/Unidade 2/Estofaria/2026/01-JANEIRO/09-pt1.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/09-pt2.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/09-pt3.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/09-pt4.jpeg"] },
             { data: "12/01/2026", captura: ["capturas/Unidade 2/Estofaria/2026/01-JANEIRO/12-pt1.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/12-pt2.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/12-pt3.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/12-pt4.jpeg"] },
             { data: "13/01/2026", captura: ["capturas/Unidade 2/Estofaria/2026/01-JANEIRO/13-pt1.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/13-pt2.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/13-pt3.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/13-pt4.jpeg"] },
-
+            { data: "14/01/2026", captura: ["capturas/Unidade 2/Estofaria/2026/01-JANEIRO/14-pt1.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/14-pt2.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/14-pt3.jpeg", "capturas/Unidade 2/Estofaria/2026/01-JANEIRO/14-pt4.jpeg"] },
             
+
+
+
             // { data: "", captura: ["", "", "", ""] },
             
         ],
