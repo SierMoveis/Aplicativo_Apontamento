9ac4cb45550be4f7e390a5af4b4e7f73aeb23eb6
SierMoveis <suportesier@gmail.com>
2026-02-03 08:50:51 -0300

    ,,

--- back__incial.js
+++ back__incial.js
@@ -554,6 +554,7 @@ const unidades = {
             { data: "27/01/2026", captura: ["capturas/Unidade 2/Acabamento/2026/01-JANEIRO/27 pt1.jpg", "capturas/Unidade 2/Acabamento/2026/01-JANEIRO/27 pt2.jpg"] },
             { data: "28/01/2026", captura: ["capturas/Unidade 2/Acabamento/2026/01-JANEIRO/28 pt1.jpg", "capturas/Unidade 2/Acabamento/2026/01-JANEIRO/28 pt2.jpg"] },
             { data: "29/01/2026", captura: ["capturas/Unidade 2/Acabamento/2026/01-JANEIRO/29 pt1.jpg", "capturas/Unidade 2/Acabamento/2026/01-JANEIRO/29 pt2.jpg"] },
+            { data: "30/01/2026", captura: ["capturas/Unidade 2/Acabamento/2026/01-JANEIRO/30 pt1.jpg", "capturas/Unidade 2/Acabamento/2026/01-JANEIRO/30 pt2.jpg"] },
 
 
 
