8456ab279c8572816072e7c232c1c0d07cf49f93
SierMoveis <suportesier@gmail.com>
2026-02-26 13:59:52 -0300

    ,,

--- back__incial.js
+++ back__incial.js
@@ -343,7 +343,9 @@ const unidades = {
             { data: "10/02/2026", captura: ["capturas/Unidade 1/2026/fevereiro/Usinagem/Filetação/10.jpeg", "capturas/Unidade 1/2026/fevereiro/cnc/10.jpeg"] },
             { data: "11/02/2026", captura: ["capturas/Unidade 1/2026/fevereiro/Usinagem/Filetação/11.jpg" , "capturas/Unidade 1/2026/fevereiro/cnc/11.jpg"] },
             { data: "23/02/2026", captura: ["capturas/Unidade 1/2026/fevereiro/Usinagem/Filetação/23.jpg" , "capturas/Unidade 1/2026/fevereiro/cnc/23.jpg"] },
+            { data: "25/02/2026", captura: ["capturas/Unidade 1/2026/fevereiro/Usinagem/Filetação/26.jpg" , "capturas/Unidade 1/2026/fevereiro/cnc/26.jpg"] },
 
+            
 
             // { data: "", captura: ["" , ""] },
 
@@ -434,6 +436,8 @@ const unidades = {
             { data: "10/02/2026", captura: ["capturas/Unidade 1/2026/fevereiro/Usinagem/Montagem/10.jpeg"] },
             { data: "13/02/2026", captura: ["capturas/Unidade 1/2026/fevereiro/Usinagem/Montagem/11.jpg"] },
             { data: "23/02/2026", captura: ["capturas/Unidade 1/2026/fevereiro/Usinagem/Montagem/23.jpg"] },
+            { data: "26/02/2026", captura: ["capturas/Unidade 1/2026/fevereiro/Usinagem/Montagem/26.jpg"] },
+
 
             // { data: "", captura: [""] },
 
@@ -750,7 +754,7 @@ const unidades = {
             { data: "10/02/2026", captura: ["capturas/Unidade 4/Usinagem/2026/Calibradora/FEVEREIRO/10.jpeg"] },
             { data: "11/02/2026", captura: ["capturas/Unidade 4/Usinagem/2026/Prensas/FEVEREIRO/19.jpg"] },
             { data: "23/02/2026", captura: ["capturas/Unidade 4/Usinagem/2026/Calibradora/FEVEREIRO/23.jpg"] },
-            { data: "26/02/2026", captura: ["capturas/Unidade 4/Usinagem/2026/Prensas/FEVEREIRO/26.jpg"] },
+            { data: "25/02/2026", captura: ["capturas/Unidade 4/Usinagem/2026/Prensas/FEVEREIRO/26.jpg"] },
 
 
 
