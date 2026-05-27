cdd155e7dae6747c39f9e0d6e0d37180a645d246
SierMoveis <suportesier@gmail.com>
2026-05-27 11:35:24 -0300

    kk

--- back__incial.js
+++ back__incial.js
@@ -380,6 +380,8 @@ const unidades = {
             { data: "21/05/2025", captura: ["capturas/Unidade 2/Usinagem/2026/05-MAIO/USINAGEM/21.JPG", "capturas/Unidade 2/Usinagem/2026/05-MAIO/MM/21.jpg"] },
             { data: "22/05/2026", captura: ["capturas/Unidade 2/Usinagem/2026/05-MAIO/USINAGEM/22.JPG", "capturas/Unidade 2/Usinagem/2026/05-MAIO/MM/22.jpg"] },
             { data: "25/05/2026", captura: ["capturas/Unidade 2/Usinagem/2026/05-MAIO/USINAGEM/25.JPG", "capturas/Unidade 2/Usinagem/2026/05-MAIO/MM/25.jpg"] },
+            { data: "26/05/2026", captura: ["capturas/Unidade 2/Usinagem/2026/05-MAIO/USINAGEM/26.JPG", "capturas/Unidade 2/Usinagem/2026/05-MAIO/MM/26.jpg"] },
+
 
 
 
@@ -463,7 +465,7 @@ const unidades = {
             { data: "21/05/2026", captura: ["capturas/Unidade 3/Usinagem/2026/05-MAIO/21.jpg"] }, 
             { data: "22/05/2026", captura: ["capturas/Unidade 3/Usinagem/2026/05-MAIO/22.jpg"] }, 
             { data: "25/05/2026", captura: ["capturas/Unidade 3/Usinagem/2026/05-MAIO/25.jpg"] }, 
-
+            { data: "26/05/2026", captura: ["capturas/Unidade 3/Usinagem/2026/05-MAIO/26.jpg"] }, 
 
             
             
