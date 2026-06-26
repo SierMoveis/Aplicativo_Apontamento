57746c8f6b7dba08c3a542f8d063e3558aa3d265
SierMoveis <suportesier@gmail.com>
2026-06-26 08:42:07 -0300

    k

--- back__incial.js
+++ back__incial.js
@@ -424,6 +424,8 @@ const unidades = {
             { data: "22/06/2026", captura: ["capturas/Unidade 2/Usinagem/2026/06-JUNHO/CNC/22.JPG"] },
             { data: "23/06/2026", captura: ["capturas/Unidade 2/Usinagem/2026/06-JUNHO/CNC/23.JPG"] },
             { data: "24/06/2026", captura: ["capturas/Unidade 2/Usinagem/2026/06-JUNHO/CNC/24.JPG"] },
+            { data: "25/06/2026", captura: ["capturas/Unidade 2/Usinagem/2026/06-JUNHO/CNC/25.JPG"] },
+
 
 
 
