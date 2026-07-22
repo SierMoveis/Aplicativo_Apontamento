e3efef7e71530c1c65ea1698951f4a7a5a35081c
SierMoveis <suportesier@gmail.com>
2026-07-22 15:33:18 -0300

    j

--- back__incial.js
+++ back__incial.js
@@ -598,7 +598,7 @@ const unidades = {
             { data: "16/07/2026", captura: ["capturas/Unidade 2/Acabamento/2026/07-JULHO/16 PT 01.jpg", "capturas/Unidade 2/Acabamento/2026/07-JULHO/16 PT 02.jpg"] },
             { data: "17/07/2026", captura: ["capturas/Unidade 2/Acabamento/2026/07-JULHO/17 PT 01.jpg", "capturas/Unidade 2/Acabamento/2026/07-JULHO/17 PT 02.jpg"] },
             { data: "20/07/2026", captura: ["capturas/Unidade 2/Acabamento/2026/07-JULHO/20 PT 01.jpg", "capturas/Unidade 2/Acabamento/2026/07-JULHO/20 PT 02.jpg"] },
-
+            { data: "21/07/2026", captura: ["capturas/Unidade 2/Acabamento/2026/07-JULHO/21 PT01.jpeg", "capturas/Unidade 2/Acabamento/2026/07-JULHO/21 PT02.jpeg"] },
 
 
             
