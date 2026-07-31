0e9adeb0263eb06b6fa345d2b953a2d42340c062
SierMoveis <suportesier@gmail.com>
2026-07-31 14:54:47 -0300

    l

--- back__incial.js
+++ back__incial.js
@@ -188,7 +188,7 @@ const unidades = {
             { data: "27/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Expedição/MetasTv/27.jpeg", "capturas/Unidade 1/2026/07 JULHO/Expedição/Embalagem/27.jpeg"] },
             { data: "28/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Expedição/MetasTv/28.jpeg", "capturas/Unidade 1/2026/07 JULHO/Expedição/Embalagem/28.jpeg"] },
             { data: "29/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Expedição/MetasTv/29.jpeg", "capturas/Unidade 1/2026/07 JULHO/Expedição/Embalagem/29.jpeg"] },
-            { data: "30/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Expedição/MetasTv/30.jpeg", "capturas/Unidade 1/2026/07 JULHO/Expedição/Embalagem/30.jpeg"] },
+
 
             // { data: "", captura: ["", ""] },
 
@@ -251,9 +251,9 @@ const unidades = {
             { data: "27/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Acabamento/27.jpeg"] },
             { data: "28/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Acabamento/28.jpeg"] },
             { data: "29/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Acabamento/29.jpeg"] },
-            { data: "30/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Acabamento/30.jpeg"] },
+            { data: "", captura: [""] },
 
-
+            
             // { data: "", captura: [""] },
 
 
@@ -319,7 +319,7 @@ const unidades = {
             { data: "27/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Usinagem - Cnc/27.jpeg"] },
             { data: "28/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Usinagem - Cnc/28.jpeg" ] },
             { data: "29/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Usinagem - Cnc/29.jpeg" ] },
-            { data: "30/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Usinagem - Cnc/30.jpeg" ] },
+
 
             // { data: "", captura: ["" ] },
 
@@ -393,7 +393,7 @@ const unidades = {
            { data: "27/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Montagem/27.jpeg"] },
            { data: "28/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Montagem/28.jpeg"] },
            { data: "29/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Montagem/29.jpeg"] },
-           { data: "30/07/2026", captura: ["capturas/Unidade 1/2026/07 JULHO/Montagem/30.jpeg"] },
+
 
             // { data: "", captura: [""] },
 
@@ -443,6 +443,7 @@ const unidades = {
             { data: "28/07/2026", captura: ["capturas/Unidade 2/Estofaria/2026/07-JULHO/28-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/07-JULHO/28-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/07-JULHO/28-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/07-JULHO/28-pt4.jpg"] },
             { data: "29/07/2026", captura: ["capturas/Unidade 2/Estofaria/2026/07-JULHO/29-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/07-JULHO/29-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/07-JULHO/29-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/07-JULHO/29-pt4.jpg"] },
             
+
 
 
 
@@ -1368,9 +1369,6 @@ const unidades = {
             { data: "27/07/2026", captura: ["capturas/Unidade 4/Tempo_parado/2026/JULHO/27MAQUIMOVEL.jpeg", "capturas/Unidade 4/Tempo_parado/2026/JULHO/27 ITALPRESSE.jpeg", "capturas/Unidade 4/Tempo_parado/2026/JULHO/27 HOME.jpeg"] },
             { data: "28/07/2026", captura: ["capturas/Unidade 4/Tempo_parado/2026/JULHO/28 HOME.jpeg", "capturas/Unidade 4/Tempo_parado/2026/JULHO/28 ITALPRESSE.jpeg", "capturas/Unidade 4/Tempo_parado/2026/JULHO/28 MAQUIMOVEL.jpeg"] },
             { data: "29/07/2026", captura: ["capturas/Unidade 4/Tempo_parado/2026/JULHO/29 HOME.jpeg", "capturas/Unidade 4/Tempo_parado/2026/JULHO/29 ITALPRESSE.jpeg", "capturas/Unidade 4/Tempo_parado/2026/JULHO/29 MAQUIMOVEL.jpeg"] },
-            { data: "30/07/2026", captura: ["capturas/Unidade 4/Tempo_parado/2026/JULHO/30 HOME.jpeg", "capturas/Unidade 4/Tempo_parado/2026/JULHO/30 ITALPRESSE.jpeg", "capturas/Unidade 4/Tempo_parado/2026/JULHO/30 MAQUIMOVEL.jpeg"] },
-
-
 
             // { data: "", captura: ["", "", ""] },
 
@@ -4263,7 +4261,7 @@ const unidades = {
             { data: "10/07/2026", captura: ["capturas/Qualidade fornecedor/vidros/2026/JULHO/10 FAST.jpg"] },
             { data: "17/07/2026", captura: ["capturas/Qualidade fornecedor/vidros/2026/JULHO/17 FAST.jpg"] },
             { data: "24/07/2026", captura: ["capturas/Qualidade fornecedor/vidros/2026/JULHO/23.jpeg"] },
-            { data: "31/07/2026", captura: ["capturas/Qualidade fornecedor/vidros/2026/JULHO/31.jpeg"] },
+
 
             // { data: "", captura: [""] },
 
@@ -4417,7 +4415,6 @@ const unidades = {
             { data: "10/07/2026", captura: ["capturas/Qualidade fornecedor/vidros/2026/JULHO/10 FAST.jpg", ""] },
             { data: "17/07/2026", captura: ["capturas/Qualidade fornecedor/vidros/2026/JULHO/17 FAST.jpg", ""] },
             { data: "24/07/2026", captura: ["capturas/Qualidade fornecedor/vidros/2026/JULHO/23.jpeg"] },
-            { data: "31/07/2026", captura: ["capturas/Qualidade fornecedor/vidros/2026/JULHO/31.jpeg"] },
             
             // { data: "", captura: ["", ""] },
 
