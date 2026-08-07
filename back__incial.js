08625ef641645aa73daadd64a2c5d5f3c453c20c
SierMoveis <suportesier@gmail.com>
2026-08-07 10:29:12 -0300

    L

--- back__incial.js
+++ back__incial.js
@@ -637,7 +637,7 @@ const unidades = {
             { data: "03/08/2026", captura: ["capturas/Unidade 2/Usinagem/2026/08-AGOSTO/USINAGEM/03.jpg", "capturas/Unidade 2/Usinagem/2026/08-AGOSTO/MM/03.jpg"] },
             { data: "04/08/2026", captura: ["capturas/Unidade 2/Usinagem/2026/08-AGOSTO/USINAGEM/04.jpg", "capturas/Unidade 2/Usinagem/2026/08-AGOSTO/MM/04.jpg"] },
             { data: "05/08/2026", captura: ["capturas/Unidade 2/Usinagem/2026/08-AGOSTO/USINAGEM/05.jpg", "capturas/Unidade 2/Usinagem/2026/08-AGOSTO/MM/05.jpg"] },
-
+            { data: "06/08/2026", captura: ["capturas/Unidade 2/Usinagem/2026/08-AGOSTO/USINAGEM/06.jpg", "capturas/Unidade 2/Usinagem/2026/08-AGOSTO/MM/06.jpg"] },
 
 
 
@@ -758,7 +758,7 @@ const unidades = {
             { data: "03/08/2026", captura: ["capturas/Unidade 3/Usinagem/2026/08-AGOSTO/03.jpg"] }, 
             { data: "04/08/2026", captura: ["capturas/Unidade 3/Usinagem/2026/08-AGOSTO/04.jpg"] }, 
             { data: "05/08/2026", captura: ["capturas/Unidade 3/Usinagem/2026/08-AGOSTO/05.jpg"] }, 
-
+            { data: "06/08/2026", captura: ["capturas/Unidade 3/Usinagem/2026/08-AGOSTO/06.jpg"] },
 
 
             
