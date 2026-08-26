c2738f340e9a1915f230441322e633eff0128a48
SierMoveis <suportesier@gmail.com>
2026-08-26 13:33:55 -0300

    K

--- back__incial.js
+++ back__incial.js
@@ -366,6 +366,7 @@ const unidades = {
             { data: "20/08/2026", captura: ["capturas/Unidade 1/2026/08 AGOSTO/Usinagem - Cnc/20.jpeg"] },
             { data: "21/08/2026", captura: ["capturas/Unidade 1/2026/08 AGOSTO/Usinagem - Cnc/21.jpeg"] },
             { data: "24/08/2026", captura: ["capturas/Unidade 1/2026/08 AGOSTO/Usinagem - Cnc/24.jpeg" ] },
+            { data: "25/08/2026", captura: ["capturas/Unidade 1/2026/08 AGOSTO/Usinagem - Cnc/25.jpeg" ] },
 
             // { data: "", captura: ["" ] },
 
@@ -454,7 +455,8 @@ const unidades = {
            { data: "20/08/2026", captura: ["capturas/Unidade 1/2026/08 AGOSTO/Montagem/20.jpeg"] },
            { data: "21/08/2026", captura: ["capturas/Unidade 1/2026/08 AGOSTO/Montagem/21.jpeg"] },
            { data: "24/08/2026", captura: ["capturas/Unidade 1/2026/08 AGOSTO/Montagem/24.jpeg" ] },
-
+           { data: "25/08/2026", captura: ["capturas/Unidade 1/2026/08 AGOSTO/Montagem/25.jpeg"] },
+           
 
             // { data: "", captura: [""] },
 
