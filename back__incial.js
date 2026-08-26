6aea12036a0779e86d0514eef66ff8d875445cfa
SierMoveis <suportesier@gmail.com>
2026-08-26 16:25:04 -0300

    g

--- back__incial.js
+++ back__incial.js
@@ -504,9 +504,11 @@ const unidades = {
             { data: "19/08/2026", captura: ["capturas/Unidade 2/Estofaria/2026/08-AGOSTO/19-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/19-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/19-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/19-pt4.jpg"] },
             { data: "20/08/2026", captura: ["capturas/Unidade 2/Estofaria/2026/08-AGOSTO/20-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/20-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/20-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/20-pt4.jpg"] },
             { data: "24/08/2026", captura: ["capturas/Unidade 2/Estofaria/2026/08-AGOSTO/24-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/24-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/24-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/24-pt4.jpg"] },
-            
+            { data: "25/08/2026", captura: ["capturas/Unidade 2/Estofaria/2026/08-AGOSTO/25-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/25-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/25-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/08-AGOSTO/25-pt4.jpg"] },
 
             
+
+
 
 
             // { data: "", captura: ["", "", "", ""] },
