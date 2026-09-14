244d167f8e67c843765c33a3d9d889c2aafb9ff4
SierMoveis <suportesier@gmail.com>
2026-09-14 14:01:48 -0300

    K

--- back__incial.js
+++ back__incial.js
@@ -582,7 +582,9 @@ const unidades = {
             { data: "08/09/2026", captura: ["capturas/Unidade 2/Expedição/2026/09-SETEMBRO/08.JPG"] },
             { data: "09/09/2026", captura: ["capturas/Unidade 2/Expedição/2026/09-SETEMBRO/09.JPG"] },
             { data: "10/09/2026", captura: ["capturas/Unidade 2/Expedição/2026/09-SETEMBRO/10.JPG"] },
-            
+            { data: "11/09/2026", captura: ["capturas/Unidade 2/Expedição/2026/09-SETEMBRO/11.JPG"] },
+
+
 
 
             // { data: "", captura: [""] },
