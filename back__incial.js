3ae597c18952ca049cc9ada65d2355cdbf6e0de9
SierMoveis <suportesier@gmail.com>
2026-09-24 16:15:27 -0300

    h

--- back__incial.js
+++ back__incial.js
@@ -259,8 +259,10 @@ const unidades = {
             { data: "18/09/2026", captura: ["capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/18-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/18-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/18-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/18-pt4.jpg"] },
             { data: "21/09/2026", captura: ["capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/21-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/21-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/21-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/21-pt4.jpg"] },
             { data: "22/09/2026", captura: ["capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/22-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/22-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/22-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/22-pt4.jpg"] },
-            
+            { data: "23/09/2026", captura: ["capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/23-pt1.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/23-pt2.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/23-pt3.jpg", "capturas/Unidade 2/Estofaria/2026/09-SETEMBRO/23-pt4.jpg"] },
 
+
+            
 
 
             // { data: "", captura: ["", "", "", ""] },
@@ -479,6 +481,7 @@ const unidades = {
             { data: "18/09/2026", captura: ["capturas/Unidade 2/Acabamento/2026/09-SETEMBRO/18-pt1.jpg", "capturas/Unidade 2/Acabamento/2026/09-SETEMBRO/18-pt2.jpg"] },
             { data: "21/09/2026", captura: ["capturas/Unidade 2/Acabamento/2026/09-SETEMBRO/21-pt1.jpg", "capturas/Unidade 2/Acabamento/2026/09-SETEMBRO/21-pt2.jpg"] },
             { data: "22/09/2026", captura: ["capturas/Unidade 2/Acabamento/2026/09-SETEMBRO/22-pt1.jpg", "capturas/Unidade 2/Acabamento/2026/09-SETEMBRO/22-pt2.jpg"] },
+            { data: "23/09/2026", captura: ["capturas/Unidade 2/Acabamento/2026/09-SETEMBRO/23-pt1.jpg", "capturas/Unidade 2/Acabamento/2026/09-SETEMBRO/23-pt2.jpg"] },
 
 
 
