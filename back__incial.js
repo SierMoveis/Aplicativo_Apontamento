d8405ddfae85f525149b0ac988b1ec0a766e2065
SierMoveis <suportesier@gmail.com>
2026-09-24 13:03:39 -0300

    ]

--- back__incial.js
+++ back__incial.js
@@ -180,6 +180,8 @@ const unidades = {
             { data: "17/09/2026", captura: ["capturas/Unidade 1/2026/09 SETEMBRO/Usinagem - Cnc/17.jpeg" ] },
             { data: "21/09/2026", captura: ["capturas/Unidade 1/2026/09 SETEMBRO/Usinagem - Cnc/21.jpeg" ] },
             { data: "22/09/2026", captura: ["capturas/Unidade 1/2026/09 SETEMBRO/Montagem/22.jpeg" ] },
+            { data: "23/09/2026", captura: ["capturas/Unidade 1/2026/09 SETEMBRO/Usinagem - Cnc/23.jpeg" ] },
+
 
             // { data: "", captura: ["" ] },
 
